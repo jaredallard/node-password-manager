34252e82abec2258dfcca3835985e5f013d764e1
Jared Allard <jaredallard@outlook.com>
2015-09-25 22:44:26 -0700

    async fixes, resolve db not being fully created on call

--- index.js
+++ index.js
@@ -15,71 +15,73 @@ var debug   = require('debug')('cli'),
 /** Internal **/
 var npm = require('./lib/npm.js');
 
-program.version(require('./package.json').version);
+npm.ee.on('init', function() {
+  program.version(require('./package.json').version);
 
-program
-  .command('init')
-  .description('Init the NPM database')
-  .action(function() {
-    // this technically does nothing.
-  });
+  program
+    .command('init')
+    .description('Init the NPM database')
+    .action(function() {
+      // this technically does nothing.
+    });
 
-program
-  .command('destroy')
-  .description('Destroy the NPM database. IRREVERSABLE!')
-  .action(function() {
-    npm.db.close(function() {
-      fs.unlink('./npm.db', function(err) {
+  program
+    .command('destroy')
+    .description('Destroy the NPM database. IRREVERSABLE!')
+    .action(function() {
+      npm.db.close(function() {
+        fs.unlink('./npm.db', function(err) {
+          if(err) {
+            console.log('Failed to destroy the database.');
+          } else {
+            console.log('He\'s dead, Jim.');
+          }
+        });
+      });
+    });
+
+  program
+    .command('new <service> <username>')
+    .description('Create a new password for service+username')
+    .action(function(cmd, options){
+      var service  = cmd,
+          username = options;
+
+      debug('add', service, username);
+      npm.addAccount(service, username, function(err, res) {
         if(err) {
-          console.log('Failed to destroy the database.');
+          debug('error occurred');
+          console.log(err);
+        } else if(res === false) {
+          debug('already exists');
+          console.log('Did you mean to `regen '+service, username+'`? That service+user already exists.');
         } else {
-          console.log('He\'s dead, Jim.');
+          debug('succedded');
+          console.log('Success! Now try `get '+service, username+'`');
         }
       });
     });
-  });
 
-program
-  .command('new <service> <username>')
-  .description('Create a new password for service+username')
-  .action(function(cmd, options){
-    var service  = cmd,
-        username = options;
+  program
+    .command('get <service> <username>')
+    .description('Get a service+usernames\' password')
+    .action(function(cmd, options){
+      var service  = cmd,
+          username = options;
 
-    debug('add', service, username);
-    npm.addAccount(service, username, function(err, res) {
-      if(err) {
-        debug('error occurred');
-        console.log(err);
-      } else if(res === false) {
-        debug('already exists');
-        console.log('Did you mean to `regen '+service, username+'`? That service+user already exists.');
-      } else {
-        debug('succedded');
-        console.log('Success! Now try `get '+service, username+'`');
-      }
+      debug('get', service, username);
+      npm.getAccount(service, username, function(err, res) {
+        if(err) {
+          debug('error occurred');
+          console.log(err);
+        } else if(res === false) {
+          debug('service+user doesn\'t work.');
+          console.log('There\'s either, a service without that user, or a user without that service!');
+        } else {
+          console.log(res.service+'\+'+res.username, '=', res.password);
+        }
+      });
     });
-  });
 
-program
-  .command('get <service> <username>')
-  .description('Get a service+usernames\' password')
-  .action(function(cmd, options){
-    var service  = cmd,
-        username = options;
-
-    debug('get', service, username);
-    npm.getAccount(service, username, function(err, res) {
-      if(err) {
-        debug('error occurred');
-        console.log(err);
-      } else if(res === false) {
-        debug('service+user doesn\'t work.');
-        console.log('There\'s either, a service without that user, or a user without that service!');
-      } else {
-        console.log(res.service+'\+'+res.username, '=', res.password);
-      }
-    });
-  });
-
-program.parse(process.argv);
+  program.parse(process.argv);
+})
